6a4d61a8c1e953bbdec1ab2da0561851853fb0a3
azamat <azamatkhabibullaevcto@gmail.com>
2025-02-20 17:02:13 +0500

    add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from '@/components/ui/button'
+
 function App() {
-  return <>App</>
+  return (
+      <>
+        <div className="flex items-center justify-center h-svh">
+          <Button>Button</Button>
+        </div>
+      </>
+  )
 }
 
 export default App
