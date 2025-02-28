5b0dd134e366cdc05766cd656622bf5f031649dc
azamat <azamatkhabibullaevcto@gmail.com>
2025-02-28 17:26:13 +0500

    add sidebar

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,10 @@
+import { AppSidebar } from "@/components/app-sidebar";
 import {
-	Link,
-	Outlet,
-	createRootRoute,
-	useMatchRoute,
-} from "@tanstack/react-router";
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Outlet, createRootRoute, useMatchRoute } from "@tanstack/react-router";
 import * as React from "react";
 
 const TanStackRouterDevtools =
@@ -32,18 +33,29 @@ function RootComponent() {
 
 	return (
 		<>
-			{!matchedNoNavRoutes ? (
-				<>
-					<div className="p-2 flex gap-2">
-						<Link to="/" className="[&.active]:font-bold">
-							Home
-						</Link>
-					</div>
-					<hr />
-					<Outlet />
-				</>
-			) : (
+			{matchedNoNavRoutes ? (
 				<Outlet />
+			) : (
+				<SidebarProvider>
+					<AppSidebar />
+					<SidebarInset>
+						<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+							<div className="flex items-center gap-2 px-4">
+								<SidebarTrigger className="-ml-1" />
+							</div>
+						</header>
+						<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+							<div className="grid auto-rows-min gap-4 md:grid-cols-3">
+								<div className="aspect-video rounded-xl bg-muted/50" />
+								<div className="aspect-video rounded-xl bg-muted/50" />
+								<div className="aspect-video rounded-xl bg-muted/50" />
+							</div>
+							<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+								<Outlet />
+							</div>
+						</div>
+					</SidebarInset>
+				</SidebarProvider>
 			)}
 			<React.Suspense fallback={null}>
 				<TanStackRouterDevtools initialIsOpen={false} position="bottom-right" />
